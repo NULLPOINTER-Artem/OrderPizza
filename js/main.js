b43f34e0be4d127dee5792b66ca21b7116c37d9a
Artem Orlov <universalism123@gmail.com>
2020-10-10 13:16:53 +0200

    Commit

--- js/main.js
+++ js/main.js
@@ -3,6 +3,10 @@ window.onload = init;
 function init() {
     let buttonToOrder = document.querySelector('.js-open-modal');
     let overlay = document.querySelector('.overlay');
+    let elements = document.querySelector('.form-order-pizza').elements;
+
+    let message = document.createElement('span');
+    message.classList.add('formForMsg');
 
     buttonToOrder.addEventListener('click', function(event) {
         event.preventDefault();
@@ -15,19 +19,14 @@ function init() {
 
         let buttonYes = modalElem.querySelector('#yes-payment');
         let buttonNo = modalElem.querySelector('#no-payment');
-
-        overlay.addEventListener('click', function (e) {
-            modalElem.classList.remove('active');
-            overlay.classList.remove('active');
-        })
+        let modalElemForLike = document.querySelector('.modal[data-modal="like"]');
 
         buttonYes.addEventListener('click', function(e) {
             e.preventDefault();
 
-            let modalElemForLike = document.querySelector('.modal[data-modal="like"]');
-            let form = document.querySelector('.form-order-pizza').elements;
+            message.classList.remove('deactive');
 
-            let objOfValues = getValues(form);
+            let objOfValues = getValues(elements);
 
             let newOrder = new Order(objOfValues);
 
@@ -38,26 +37,24 @@ function init() {
 
             document.querySelector('.form-order-pizza').classList.add('hide');
 
-            let cook = document.createElement('span');
-            cook.classList.add('formForMsg');
-            cook.textContent = 'Cooking...';
+            message.textContent = 'Cooking...';
             
-            document.querySelector('.form-order-pizza').before(cook);
+            document.querySelector('.form-order-pizza').before(message);
 
             setTimeout(function() {
-                cook.textContent = 'Deliver took your pizza!';
+                message.textContent = 'Deliver took your pizza!';
             }, (3 * 1000));
 
             newOrder.status = 'cooked';
 
             setTimeout(function() {
-                cook.textContent = 'Deliver delivered your pizza!';
+                message.textContent = 'Deliver delivered your pizza!';
             }, (5 * 1000));
 
             newOrder.status = 'delivered';
 
             setTimeout(function() {
-                cook.classList.add('deactive');
+                message.classList.add('deactive');
                 modalElemForLike.classList.add('active');
                 overlay.classList.add('active');
             }, (7 * 1000));
@@ -68,7 +65,37 @@ function init() {
             buttonYesForLike.addEventListener('click', function(e) {
                 e.preventDefault();
 
-                
+                modalElemForLike.classList.remove('active');
+                overlay.classList.remove('active');
+
+                message.textContent = "Thank You for your feedback!";
+                message.classList.remove('deactive');
+
+
+                setTimeout(function() {
+                    message.classList.add('deactive')
+                    document.querySelector('.form-order-pizza').classList.remove('hide');
+                }, (3 * 1000));
+
+                setDefault(elements);
+            });
+
+            buttonNoForLike.addEventListener('click', function (e) {
+                e.preventDefault();
+
+                modalElemForLike.classList.remove('active');
+                overlay.classList.remove('active');
+
+                message.textContent = "Thank You for your feedback!";
+                message.classList.remove('deactive');
+
+
+                setTimeout(function () {
+                    message.classList.add('deactive')
+                    document.querySelector('.form-order-pizza').classList.remove('hide');
+                }, (3 * 1000));
+
+                setDefault(elements);
             });
         })
 
@@ -77,15 +104,24 @@ function init() {
 
             modalElem.classList.remove('active');
             overlay.classList.remove('active');
+
+            message.textContent = "The payment did not pass!";
+
+            document.querySelector('.js-open-modal').before(message);
+        })
+
+        overlay.addEventListener('click', function (e) {
+            modalElem.classList.remove('active');
+            overlay.classList.remove('active');
         })
     })
 }
 
 
-function getValues(form) {
+function getValues(elements) {
     let types = ['radio', 'checkbox'];
 
-    let correctInputs = [].filter.call(form, (item) => types.includes(item.type))
+    let correctInputs = [].filter.call(elements, (item) => types.includes(item.type))
         .filter((item) => item.checked).map( (item) => item.value);
     
     [size, ...ingredients] = correctInputs;
@@ -94,4 +130,18 @@ function getValues(form) {
         size,
         ingredients,
     }
+}
+
+function setDefault(elements) {
+    let types = ['radio', 'checkbox'];
+
+    [].forEach.call(elements, (item) => {
+        if(types.includes(item.type)) {
+            item.checked = false;
+            if (item.value === 'medium') {
+                console.log(item);
+                item.checked = true
+            }
+        }
+    })
 }
